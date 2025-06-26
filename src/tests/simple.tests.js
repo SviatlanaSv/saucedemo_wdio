a59d76fd42cfe48c347c748e1ac26a59638bac5d
Sviatlana Svarabovic <sviatlana.svarabovic@gmail.com>
2025-06-26 14:43:16 +0300

    added POM UC2

--- src/tests/simple.tests.js
+++ src/tests/simple.tests.js
@@ -9,7 +9,6 @@ describe('Login functionality', () => {
     });
 
     it('UC-1: should show error if fields are empty', async () => {
-
         await loginPage.username.setValue(users.invalid.username);
         await loginPage.password.setValue(users.invalid.password);
 
@@ -27,21 +26,19 @@ describe('Login functionality', () => {
 
 
     it('UC-2: should show error if password is missing (valid user)', async () => {
-        await $('#user-name').setValue(users.valid.username);
-        await $('#password').setValue(users.valid.password);
+        await loginPage.username.setValue(users.valid.username);
+        await loginPage.password.setValue(users.valid.password);
 
-        // await clearInput('#password');
         await loginPage.password.clearValue();
 
-        console.log('Password field after manual backspace should be empty:', await $('#password').getValue());
+        console.log('Password field after clear should be empty:', await loginPage.password.getValue());
 
-        await $('#login-button').click();
+        await loginPage.loginBtn.click();
 
-        await expect($('h3[data-test="error"]')).toBeDisplayed();
-        await expect($('h3[data-test="error"]')).toHaveText(
-            expect.stringContaining('Password is required')
-        );
+        await expect(loginPage.errorMsg).toBeDisplayed();
+        await expect(loginPage.errorMsg).toHaveText(expect.stringContaining('Password is required'));
     });
+    
 
     it('UC-3: should log in with valid username and password and see "Swag Labs"', async () => {
         await $('#user-name').setValue(users.valid.username);
